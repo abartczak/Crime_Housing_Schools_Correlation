81f5a45911e1f1c6c3d6165c878208a4cadcd521
Andrew Bartczak <bartczaka@gmail.com>
2020-04-25 11:23:12 -0500

    Pushing School Data into github

---
+++ School_Data/config.py
@@ -0,0 +1,7 @@
+# Enter your API keys for schooldigger.com
+appID = '3a0b9323'
+appKey = '9a2595b34c6fb883cf5123131bc8a43f'
+
+# Enter your API keys for GoogleMaps keys
+gkey = "AIzaSyCn2kT6DJgTgVQoCAVzopnTYw4x0eC4Xis"
+ 
